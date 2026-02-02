c8c1ad44af27a6c122a4aed4da1f9c111164c67f
Otto Hermann <ojhermann@gmail.com>
2026-02-02 14:05:14 -0500

    helix-nix

--- home_pkgs/git.nix
+++ home_pkgs/git.nix
@@ -1,17 +1,17 @@
-{ configs, pgks, ...}:
+{ configs, pgks, ... }:
 {
   programs.git = {
     enable = true;
     userName = "Otto Hermann";
     userEmail = "ojhermann@gmail.com";
+    aliases = {
+      cb = "!git rev-parse --abbrev-ref HEAD | pbcopy";
+      up = "!git remote update -p; git merge --ff-only @{u}";
+    };
     extraConfig = {
       init.defaultBranch = "main";
       pull.ff = "only";
       push.autoSetupRemote = true;
-      alias = {
-        cb = "!git rev-parse --abbrev-ref HEAD | pbcopy";
-        up = "!git remote update -p; git merge --ff-only @{u}";
-      };
     };
   };
 }

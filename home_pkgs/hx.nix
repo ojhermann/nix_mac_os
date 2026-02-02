8a3eda611fa89fd89ef962bc2969c239aa2d4d7c
Otto Hermann <ojhermann@gmail.com>
2026-02-02 13:57:50 -0500

    helix-nix

--- home_pkgs/hx.nix
+++ home_pkgs/hx.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ...}:
 {
-  home.packages = [pkgs.helix];
+  home.packages = [
+    pkgs.helix
+    # nix
+    pkgs.nil
+    pkgs.nixd
+    pkgs.nixfmt
+  ];
   
   programs.helix = {
     enable = true;
@@ -9,6 +15,18 @@
       theme = "default";
       editor.file-picker.hidden = false;
       editor.lsp.display-inlay-hints = true;
+    };
+
+    languages = {
+      language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter = {
+          command = "nixfmt";
+        };
+      }
+      ];
     };
   
   };
